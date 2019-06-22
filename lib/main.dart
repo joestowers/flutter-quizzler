87befa24ecfd66530e40d0a0d3de06482fcf48fb
Joseph Stowers <joseph.stowers@resideo.com>
2019-06-22 11:40:17 -0500

    Add question counter

--- lib/main.dart
+++ lib/main.dart
@@ -26,11 +26,16 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+  int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
         Expanded(
@@ -39,7 +44,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[_counter],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -63,6 +68,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                print('counter = $_counter');
                 setState(() {
                   scoreKeeper.add(
                     Icon(
@@ -70,6 +76,11 @@ class _QuizPageState extends State<QuizPage> {
                       color: Colors.green
                     )
                   );
+                  if (_counter == questions.length - 1) {
+                    _counter = 0;
+                  } else {
+                    _counter +=1;
+                  }
                 });
               },
             ),
@@ -89,7 +100,12 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  scoreKeeper.add(Icon(Icons.close, color: Colors.red));  
+                  scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+                  if (_counter == questions.length - 1) {
+                    _counter = 0;
+                  } else {
+                    _counter +=1;
+                  }  
                 });
               },
             ),
